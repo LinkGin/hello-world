2669e55a20015e883b4352e31e26bb8ada146ecf
akali <1261029299@qq.com>
2026-03-12 16:44:44 +0800

    Add lowest score display

--- score.py
+++ score.py
@@ -1,6 +1,8 @@
 scores = [78, 85, 92, 66, 88]
 average = sum(scores) / len(scores)
 highest = max(scores)
+lowest = min(scores)
 
 print("Average score:", average)
 print("Highest score:", highest)
+print("Lowest score:", lowest)
